b01d25ad7307ef16152fd28d86f6b9e3198f8dd0
amresh-maisonette <120711949+amresh-maisonette@users.noreply.github.com>
2023-04-11 19:10:19 +0530

    Create airbyte_connector_dag.py

---
+++ dags/airbyte_connector_dag.py
@@ -0,0 +1,35 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
+from airflow.providers.airbyte.sensors.airbyte import AirbyteJobSensor
+from airflow.sensors.filesystem import FileSensor
+import pendulum
+
+AIRBYTE_CONNECTION_ID = '91fb5891-7739-48aa-b03a-eb880aad839e'
+
+with DAG(dag_id='klaviyo-sync-postgress',
+        default_args={'owner': 'airflow'},
+        schedule='@daily',
+        start_date=pendulum.today('UTC').add(days=-1)
+   ) as dag:
+
+   trigger_airbyte_sync = AirbyteTriggerSyncOperator(
+       task_id='airbyte_trigger_sync',
+       airbyte_conn_id='airbyteconnection',
+       connection_id=AIRBYTE_CONNECTION_ID,
+       asynchronous=True
+   )
+
+   wait_for_sync_completion = AirbyteJobSensor(
+       task_id='airbyte_check_sync',
+       airbyte_conn_id='airbyteconnection',
+       airbyte_job_id=trigger_airbyte_sync.output
+   )
+
+   trigger_airbyte_sync >> wait_for_sync_completion 
+
+
+
+
+
+
