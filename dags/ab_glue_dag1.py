f75c639e2cb2a44576d529c11feeb425fc167160
shruti090497 <110972540+shruti090497@users.noreply.github.com>
2023-04-13 15:13:16 +0530

    Update ab_glue_dag1.py

--- dags/ab_glue_dag1.py
+++ dags/ab_glue_dag1.py
@@ -8,32 +8,45 @@ from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
 
 import pendulum
 
-AIRBYTE_CONNECTION_ID = '91fb5891-7739-48aa-b03a-eb880aad839e'
 
-with DAG(dag_id='klaviyo-sync-postgress-glue1',
-        default_args={'owner': 'airflow'},
-        schedule='@daily',
-        start_date=pendulum.today('UTC').add(days=-1)
-   ) as dag:
+submit_glue_job = GlueJobOperator(
+    task_id="submit_glue_job",
+    job_name="Data-Flatten-klaviyo",
+    script_location=f"s3://maisonette-airbyte-integration-landing-dev/Flattened-data-glue-dag/dataflatten.py",
+    s3_bucket="maisonette-airbyte-integration-landing-dev",
+    iam_role_name=role_name,
+    create_job_kwargs={"GlueVersion": "3.0", "NumberOfWorkers": 2, "WorkerType": "G.1X"},
+)
 
-#    trigger_airbyte_sync = AirbyteTriggerSyncOperator(
-#        task_id='airbyte_trigger_sync',
-#        airbyte_conn_id='airbyteconnection',
-#        connection_id=AIRBYTE_CONNECTION_ID,
-#        asynchronous=True
+
+
+
+# AIRBYTE_CONNECTION_ID = '91fb5891-7739-48aa-b03a-eb880aad839e'
+
+# with DAG(dag_id='klaviyo-sync-postgress-glue1',
+#         default_args={'owner': 'airflow'},
+#         schedule='@daily',
+#         start_date=pendulum.today('UTC').add(days=-1)
+#    ) as dag:
+
+# #    trigger_airbyte_sync = AirbyteTriggerSyncOperator(
+# #        task_id='airbyte_trigger_sync',
+# #        airbyte_conn_id='airbyteconnection',
+# #        connection_id=AIRBYTE_CONNECTION_ID,
+# #        asynchronous=True
+# #    )
+
+# #    wait_for_sync_completion = AirbyteJobSensor(
+# #        task_id='airbyte_check_sync',
+# #        airbyte_conn_id='airbyteconnection',
+# #        airbyte_job_id=trigger_airbyte_sync.output
+# #    )
+
+#    trigger_glue_job = GlueJobOperator(
+#        task_id='trigger_glue_job',
+#        job_name='Data-Flatten-klaviyo',
+#        aws_conn_id='airbyte_glue_data_flattening',  # replace with your AWS connection ID
+#        region_name='us-west-2'  # replace with your AWS region
 #    )
 
-#    wait_for_sync_completion = AirbyteJobSensor(
-#        task_id='airbyte_check_sync',
-#        airbyte_conn_id='airbyteconnection',
-#        airbyte_job_id=trigger_airbyte_sync.output
-#    )
-
-   trigger_glue_job = GlueJobOperator(
-       task_id='trigger_glue_job',
-       job_name='Data-Flatten-klaviyo',
-       aws_conn_id='airbyte_glue_data_flattening',  # replace with your AWS connection ID
-       region_name='us-west-2'  # replace with your AWS region
-   )
-
-trigger_glue_job
+# trigger_glue_job
