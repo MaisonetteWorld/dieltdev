9fd65d51d7fd0d5d242f67969522d786f6145b3a
asifkhan-cdt <110971932+asifkhan-cdt@users.noreply.github.com>
2023-04-13 22:58:15 +0530

    Update ab_glue_dag1.py

--- dags/ab_glue_dag1.py
+++ dags/ab_glue_dag1.py
@@ -1,35 +1,25 @@
 from airflow import DAG
-from airflow.operators.email_operator import EmailOperator
-from airflow.providers.amazon.aws.operators.glue import AwsGlueJobOperator
-from datetime import datetime, timedelta
+from airflow.operators.bash import BashOperator
+from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
+from airflow.providers.airbyte.sensors.airbyte import AirbyteJobSensor
+from airflow.sensors.filesystem import FileSensor
+from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
+import pendulum
+#AIRBYTE_CONNECTION_ID = '91fb5891-7739-48aa-b03a-eb880aad839e'
 
+with DAG(dag_id='klaviyo-sync-postgress-glue1',
+        default_args={'owner': 'airflow'},
+        schedule='@daily',
+        start_date=pendulum.today('UTC').add(days=-1)
+   ) as dag:
 
-### glue job specific variables
-glue_job_name = "data_ingestion_rds"
-glue_iam_role = "integration-glue-role"
-region_name = "us-west-2"
-email_recipient = "asif.khan@coditas.com"
-
-default_args = {
-    'owner': 'me',
-    'start_date': datetime(2020, 1, 1),
-    'retry_delay': timedelta(minutes=5),
-    'email': email_recipient,
-    'email_on_failure': True
-}
-
-
-with DAG(dag_id = 'klaviyo-sync-postgress-glue1', default_args = default_args, schedule_interval = None) as dag:
-    
-    glue_job_step = AwsGlueJobOperator(
-        job_name =glue_job_name,
-        script_location = 's3://my-s3-location',
-        region_name = region_name,
-        iam_role_name = glue_iam_role,
-        script_args=None,
-        num_of_dpus=10,
-        task_id = 'glue_job_step',
-        dag = dag
-        )
-   
-    glue_job_step
+submit_glue_job = GlueJobOperator(
+    task_id="submit_glue_job",
+    job_name="Data-Flatten-klaviyo",
+    script_location=f"s3://maisonette-airbyte-integration-landing-dev/Flattened-data-glue-dag/maisonette_codes-0.0.1-py3-none-any.whl",
+    s3_bucket="maisonette-airbyte-integration-landing-dev",
+    iam_role_name=data-integration-glue-role,
+    create_job_kwargs={"GlueVersion": "3.0", "NumberOfWorkers": 2, "WorkerType": "G.1X"},
+)
+# trigger_glue_job
+submit_glue_job
