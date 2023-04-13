48c19b78c1406ff71055c6e73be99b4e62573bbf
asifkhan-cdt <110971932+asifkhan-cdt@users.noreply.github.com>
2023-04-13 16:22:25 +0530

    Update ab_glue_dag1.py

--- dags/ab_glue_dag1.py
+++ dags/ab_glue_dag1.py
@@ -4,30 +4,15 @@ from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperat
 from airflow.providers.airbyte.sensors.airbyte import AirbyteJobSensor
 from airflow.sensors.filesystem import FileSensor
 from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
-
-
 import pendulum
-
-
-submit_glue_job = GlueJobOperator(
-    task_id="submit_glue_job",
-    job_name="Data-Flatten-klaviyo",
-    script_location=f"s3://maisonette-airbyte-integration-landing-dev/Flattened-data-glue-dag/dataflatten.py",
-    s3_bucket="maisonette-airbyte-integration-landing-dev",
-    iam_role_name=role_name,
-    create_job_kwargs={"GlueVersion": "3.0", "NumberOfWorkers": 2, "WorkerType": "G.1X"},
-)
-
-
-
 
 # AIRBYTE_CONNECTION_ID = '91fb5891-7739-48aa-b03a-eb880aad839e'
 
-# with DAG(dag_id='klaviyo-sync-postgress-glue1',
-#         default_args={'owner': 'airflow'},
-#         schedule='@daily',
-#         start_date=pendulum.today('UTC').add(days=-1)
-#    ) as dag:
+with DAG(dag_id='klaviyo-sync-postgress-glue1',
+        default_args={'owner': 'airflow'},
+        schedule='@daily',
+        start_date=pendulum.today('UTC').add(days=-1)
+   ) as dag:
 
 # #    trigger_airbyte_sync = AirbyteTriggerSyncOperator(
 # #        task_id='airbyte_trigger_sync',
@@ -49,4 +34,14 @@ submit_glue_job = GlueJobOperator(
 #        region_name='us-west-2'  # replace with your AWS region
 #    )
 
+submit_glue_job = GlueJobOperator(
+    task_id="submit_glue_job",
+    job_name="Data-Flatten-klaviyo",
+    script_location=f"s3://maisonette-airbyte-integration-landing-dev/Flattened-data-glue-dag/maisonette_codes-0.0.1-py3-none-any.whl",
+    s3_bucket="maisonette-airbyte-integration-landing-dev",
+    iam_role_name='data-integration-glue-role',
+    create_job_kwargs={"GlueVersion": "3.0", "NumberOfWorkers": 2, "WorkerType": "G.1X"},
+)
+
 # trigger_glue_job
+submit_glue_job
