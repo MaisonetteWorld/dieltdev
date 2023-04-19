93bce9de563f5a6313a97ddd728fd229e893f989
shruti090497 <110972540+shruti090497@users.noreply.github.com>
2023-04-19 18:04:38 +0530

    Update python_script_test_dag.py

--- dags/python_script_test_dag.py
+++ dags/python_script_test_dag.py
@@ -13,7 +13,7 @@ with DAG(dag_id='my_python_script_dag',
 
    run_python_script = BashOperator(
       task_id= 'run_python_script',
-      bash_command= 'python scripts/test_python_script.py'
+      bash_command= 'python scripts/test_python_script.py',
       dag=dag
    )
 
