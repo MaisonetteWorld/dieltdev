6267496f6a3a9e522f429e9fe1a8008c8ac04878
asifkhan-cdt <110971932+asifkhan-cdt@users.noreply.github.com>
2023-04-21 16:39:37 +0530

    Add files via upload

--- dags/airbyte_lambda_glue.py
+++ dags/airbyte_lambda_glue.py
@@ -5,40 +5,34 @@ from airflow.providers.airbyte.sensors.airbyte import AirbyteJobSensor
 import pendulum
 import json
 
-
-
-
 AIRBYTE_CONNECTION_ID = '91fb5891-7739-48aa-b03a-eb880aad839e'
 
 with DAG(
-    dag_id='airbyte_slasify_glue', 
-    default_args={'owner': 'airflow'}, 
-    schedule='@daily', 
-    start_date=pendulum.today('UTC').add(days=-1)
-    ) as dag:
-    
+        dag_id='airbyte_slasify_glue',
+        default_args={'owner': 'airflow'},
+        schedule='@daily',
+        start_date=pendulum.today('UTC').add(days=-1)
+) as dag:
     trigger_airbyte_sync = AirbyteTriggerSyncOperator(
-       task_id='airbytsalsify_trigger_sync',
-       airbyte_conn_id='airbyteconnection',
-       connection_id=AIRBYTE_CONNECTION_ID,
-       asynchronous=True
-   )
+        task_id='airbytsalsify_trigger_sync',
+        airbyte_conn_id='airbyteconnection',
+        connection_id=AIRBYTE_CONNECTION_ID,
+        asynchronous=True
+    )
 
-   wait_for_sync_completion = AirbyteJobSensor(
-       task_id='airbyte_salsify_check_sync',
-       airbyte_conn_id='airbyteconnection',
-       airbyte_job_id=trigger_airbyte_sync.output
-   )
-
-    
+    wait_for_sync_completion = AirbyteJobSensor(
+    task_id='airbyte_salsify_check_sync',
+    airbyte_conn_id='airbyteconnection',
+    airbyte_job_id=trigger_airbyte_sync.output
+    )
 
     invoke_lambda_function = LambdaInvokeFunctionOperator(
-        task_id="invoke_lambda_function",
-        function_name="data-integration-landing",
-        payload=json.dumps({"key1" : "value1"}),
-        aws_conn_id="airbyte_aws_data_flattening",
+    task_id="invoke_lambda_function",
+    function_name="data-integration-landing",
+    payload=json.dumps({"key1": "value1"}),
+    aws_conn_id="airbyte_aws_data_flattening",
 
-        dag=dag
+    dag=dag
     )
 
     trigger_airbyte_sync >> wait_for_sync_completion >> invoke_lambda_function
