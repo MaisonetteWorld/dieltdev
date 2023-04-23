60dd52f305d5af8fd47a7b1fc4b7b2aaa2d1dc59
shruti090497 <110972540+shruti090497@users.noreply.github.com>
2023-04-23 13:16:18 +0530

    Update mydem.py

--- dags/mydem.py
+++ dags/mydem.py
@@ -73,4 +73,42 @@ with DAG(dag_id=DAG_ID, default_args=default_args, schedule_interval=None) as da
         python_callable=process_json_files
     )
 
-    process_json
+
+
+    def combine_json_files():
+        s3_hook = S3Hook(aws_conn_id='airbyte_glue_data_flattening')  # Make sure to create a connection to AWS in Airflow
+        source_bucket_name = 'maisonette-airbyte-integration-landing-dev'
+        source_prefix = 'output_folder/'
+        destination_bucket_name = 'maisonette-airbyte-integration-landing-dev'
+        destination_prefix = 'combined_folder/'
+        destination_key = f"{destination_prefix}combined.json"
+        
+        combined = []
+        for json_file in s3_hook.list_keys(bucket_name=source_bucket_name, prefix=source_prefix):
+            file_content = s3_hook.read_key(key=json_file, bucket_name=source_bucket_name)
+            if file_content.strip() == "":
+                # Skip empty files
+                continue
+            try:
+                combined.append(json.loads(file_content))
+            except json.decoder.JSONDecodeError:
+                # Skip malformed files
+                print(f"Skipping malformed file: {json_file}")
+
+        combined_json = json.dumps(combined).encode('utf-8')  # Convert string data to bytes
+        s3_hook.load_bytes(
+            bytes_data=combined_json,
+            key=destination_key,
+            bucket_name=destination_bucket_name,
+            replace=True
+        )
+
+        print(f"Combined JSON data saved to S3 bucket {destination_bucket_name} with key {destination_key}")
+
+    combine_files = PythonOperator(
+        task_id="combine_files",
+        python_callable=combine_json_files
+    )
+
+    
+    process_json >> combine_files
